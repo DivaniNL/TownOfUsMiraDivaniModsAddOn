97f8ab634af759ca9c02792b37484114096389a3
espeon3 <espeonicalic2@gmail.com>
2026-08-10 11:20:37 +0200

    Finished!!

---
+++ Modifiers/Impostor/DeathnoteNoteModifier.cs
@@ -0,0 +1,9 @@
+﻿using MiraAPI.Modifiers;
+
+namespace DivaniMods.Modifiers.Impostors;
+
+public class BrawlerModifier : BaseModifier
+{
+    public override string ModifierName => "Noted";
+    public override bool HideOnUi => true;
+}
