900b150cbe76e9371ef6032f0c1b80d3801af4de
espeon3 <espeonicalic2@gmail.com>
2026-08-10 11:17:00 +0200

    event

---
+++ Events/Impostor/ImpostorKilling/DeathnoteEvents.cs
@@ -0,0 +1,22 @@
+using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Player;
+
+public class DeathNoteEvents
+{
+    [RegisterEvent]
+    public static void OnPlayerDeath(PlayerDeathEvent @event)
+    {
+        if (!AmongUsClient.Instance.AmHost) return;
+
+        var dead = @event.Player
+
+        if (dead.Data.Role is not DeathNoteRole) return;
+
+   foreach (var player in Helpers.GetAlivePlayers())
+{
+    if player.HasModifier<NoteModifier>
+    if (player.Data.Role.IsImpostor) continue;
+    PlayerControl.LocalPlayer.RpcCustomMurder(player, true, teleportMurderer:false);
+}
+}
+}
